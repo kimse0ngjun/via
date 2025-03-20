5a04edd14b2e9c623b2c4887d6208ccecceea342
sewon <swon7150@gmail.com>
2025-03-20 16:43:04 +0900

    로고 및 벡터 이미지 파일 삭제, AppHeader에서 로고 파일을 SVG로 변경 및 스타일 개선

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { Layout } from "antd";
 import { Link } from "react-router-dom";
-import { ReactComponent as ArrowRight } from "../assets/arrow-right.svg";
-import VIACom from "../assets/logo.png";
+import VIACom from "../assets/via_logo.svg";
 import line2 from "../assets/line-2.svg";
 
 import "../styles/AppHeader.css";
@@ -16,42 +15,34 @@ const AppHeader = () => {
     <Layout>
       <Header className="header">
         <div className="box">
-          <div className="overlap">
-            <Link to="/login">
-              <button className="login-btn">
-                <div className="text-wrapper">login</div>
-              </button>
-            </Link>
+          <Link to="/login" className="login-btn">
+            <div className="overlap-group">로그인</div>
+          </Link>
 
-            <Link to="/signup">
-              <div className="signup-btn">
-                <div className="overlap-group">
-                  <div className="vuesax-linear-arrow-wrapper">
-                    <ArrowRight className="vuesax-linear-arrow" />
-                  </div>
-                  <div className="div">signup</div>
-                </div>
-              </div>
-            </Link>
-
-            <img className="line" alt="Line" src={line2} />
-
-            <Link to="/">
-              <img className="VIA-com" alt="VIA Logo" src={VIACom} />
-            </Link>
-
-            <div className="navbar">
-              {["진로", "면접", "관심분야", "상담기록"].map((label, index) => (
-                <div
-                  key={index}
-                  className={`nav-item ${activeIndex === index ? "active" : ""}`}
-                  onMouseEnter={() => setActiveIndex(index)}
-                  onMouseLeave={() => setActiveIndex(null)}
-                >
-                  <div className="nav-text">{label}</div>
-                </div>
-              ))}
+          <Link to="/signup" className="signup-btn">
+            <div className="overlap-group">
+              
+              <div className="div">회원가입</div>
             </div>
+          </Link>
+
+          <img className="line" alt="Line" src={line2} />
+
+          <Link to="/">
+            <img className="VIA-com" alt="VIA Logo" src={VIACom} />
+          </Link>
+
+          <div className="navbar">
+            {["진로", "면접", "관심분야", "상담기록"].map((label, index) => (
+              <div
+                key={index}
+                className={`nav-item ${activeIndex === index ? "active" : ""}`}
+                onMouseEnter={() => setActiveIndex(index)}
+                onMouseLeave={() => setActiveIndex(null)}
+              >
+                <div className="nav-text">{label}</div>
+              </div>
+            ))}
           </div>
         </div>
       </Header>
