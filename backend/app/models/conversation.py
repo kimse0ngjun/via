5422b842514237a70db5fe7ddccb03bf25eb3782
kimse0ngjun <tjdwns8756@naver.com>
2025-04-10 16:18:19 +0900

    chat, conversation 모델 수정

--- backend/app/models/conversation.py
+++ backend/app/models/conversation.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
 from datetime import datetime, timezone
-from uuid import uuid4
 
-class ChatCreate(BaseModel):
-    con_id: str
-    email: str
-    user_message: str
+# 대화 모델
+class ConversationCreate(BaseModel):
+    user_id: str
+    # topic: str
     created_at: datetime = datetime.now(timezone.utc)
+    is_deleted: bool = False
